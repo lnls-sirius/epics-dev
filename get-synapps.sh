246b8a542c7997106c5affb719ce2239c2344910
Lucas Russo <lerwys@gmail.com>
2017-12-14 09:49:59 -0200

    get-synapps.sh: add additional get-ioc-stats script for download only
    Without the extra call to get-ioc-stats, the IOCStats
    module will not download as it's called only after SynApps
    installation

--- get-synapps.sh
+++ get-synapps.sh
@@ -26,6 +26,9 @@ EPICS_SYNAPPS=${EPICS_FOLDER}/synApps_${SYNAPPS_VERSION}/support
 
 if [ "${DOWNLOAD_APP}" == "yes" ]; then
     wget -nc http://www.aps.anl.gov/bcda/synApps/tar/synApps_${SYNAPPS_VERSION}.tar.gz
+    # Run IOC Stats script here only for download. For installation it will run
+    # after SynApps "make release" command
+    DOWNLOAD_APP=yes INSTALL_APP=no CLEANUP_APP=no ./get-ioc-stats.sh
 fi
 
 ########################### EPICS synApps modules ##############################
