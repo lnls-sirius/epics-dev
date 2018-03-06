b01a439f21723585c3c8a2490391ecef8bcad400
Lucas Russo <lerwys@gmail.com>
2018-03-06 17:34:09 -0300

    get-synapps.sh: add new autosave to synapps installation

--- get-synapps.sh
+++ get-synapps.sh
@@ -35,6 +35,7 @@ function get_ioc_modules () {
     DOWNLOAD_APP=${download_app} INSTALL_APP=${install_app} CLEANUP_APP=${cleanup_app} ./get-caput-recorder.sh
     DOWNLOAD_APP=${download_app} INSTALL_APP=${install_app} CLEANUP_APP=${cleanup_app} ./get-seq.sh
     DOWNLOAD_APP=${download_app} INSTALL_APP=${install_app} CLEANUP_APP=${cleanup_app} ./get-ioc-stats.sh
+    DOWNLOAD_APP=${download_app} INSTALL_APP=${install_app} CLEANUP_APP=${cleanup_app} ./get-autosave.sh
 }
 
 if [ "${DOWNLOAD_APP}" == "yes" ]; then
